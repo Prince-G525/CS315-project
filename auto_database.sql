e38f94f66feb863cc85233ecd2763df7c43c5438
Prince Gaurav <princ@iitk.ac.in>
2018-04-15 00:37:54 +0530

    Doing sth

--- auto_database.sql
+++ auto_database.sql
@@ -1,4 +1,5 @@
 CREATE TABLE auto(
+	id CHAR(10) PRIMARY KEY,
 	name CHAR(20) NOT NULL,
 	type CHAR(10) NOT NULL,
 	afrom CHAR(20) NOT NULL,
@@ -13,7 +14,17 @@ CREATE TABLE traveller(
 	contact CHAR(10) NOT NULL,
 	dob CHAR(20) ,
 	address CHAR(30) ,
-	email CHAR(20)
+	email CHAR(20),
+	PRIMARY KEY(name, contact)
 );
 
+CREATE TABLE request(
+	name CHAR(20) NOT NULL,
+	contact CHAR(10) NOT NULL,
+	pickfrom CHAR(20) NOT NULL,
+	type CHAR(10) NOT NULL,
+	route CHAR(40) NOT NULL,
+	time CHAR(20) NOT NULL,
+	FOREIGN KEY(name,contact) REFERENCES traveller(name,contact)
+);
 
